11f2c59566939c07cd9e7775c1e4a40278c46b38
Your Name <you@example.com>
2019-09-19 14:21:48 -0700

    added more ways to test end points

--- server.spec.js
+++ server.spec.js
@@ -1,24 +1,40 @@
+//using 3 ways: returen, async await, done
+
 const request = require('supertest');
 
 const server = require('./server');
 
 describe('server.js', ()=> {
-    describe('index route', ()=> {
-        it('should return an OK status', async ()=> {
-            const expectedStatusCode = 200;
-            const response = await request(server).get('/');
-            expect(response.status).toEqual(expectedStatusCode);
+    describe('GET /', ()=> {
+        it('should return 200 OK status', 
+            // async 
+            ()=> {
+            return request(server) //return is very important here
+                .get('/')
+                .then(res => {
+                    expect(res.status).toBe(200)
+                })
+            // const expectedStatusCode = 200;
+            // const response = await request(server).get('/');
+            // expect(response.status).toEqual(expectedStatusCode);
         });
 
         it('should return it is working', async ()=> {
-            const expectedBody = "it's working!!!"
-            const response = await request(server).get('/');
-            expect(response.body).toEqual(expectedBody);
+            const res = await request(server).get('/');
+            expect(res.body).toEqual("it's working!!!");
         })
 
-        it('should return a JSON object from the index route', async ()=> {
-            const response = await request(server).get('/')
-            expect(response.type).toEqual('application/json')
+        it('should return a JSON object from the index route', 
+        // async 
+        ()=> {
+            // const response = await request(server).get('/')
+            // expect(response.type).toEqual('application/json')
+            request(server)
+                .get('/')
+                .then(res => {
+                    expect(res.type).toMatch(/json/i)
+                    done();
+                })
         })
     })
 })
